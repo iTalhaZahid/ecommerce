ae1466c653b235398f4d3edf4dcd84a3092b6945
Talha Zahid <italhazahid@gmail.com>
2026-01-31 13:00:40 +0500

    Deployment Commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,24 @@
 import express from 'express';
-
+import path from 'path';
+import {ENV} from './config/env.js';
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({message:'OK!'});
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+//make app ready for deployment
+if(ENV.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname,'../admin/dist')));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname,'../admin/dist/index.html'));
+  });
+}
+ 
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on http://localhost:${ENV.PORT}`);
 });
