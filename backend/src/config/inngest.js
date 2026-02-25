b5ce9267b415409d5daa6c22448a4197563d4435
Talha Zahid <italhazahid@gmail.com>
2026-02-25 15:05:58 +0500

    Added Create Function for Adding New User

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,28 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "ecommerce-app" });
+
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
+    //for mongoose, we need to create a new instance of the model and then save it to the database
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "User",
+      imageUrl: image_url,
+    };
+
+    //Now we can create a new user document and save it to the database
+    await User.create(newUser);
+  },
+);
+
+export const functions = [syncUser];
