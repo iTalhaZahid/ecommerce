429ff3167955701b30928efc5169f9b399491601
Talha Zahid <italhazahid@gmail.com>
2026-03-01 16:00:30 +0500

    Addded Multer Middleware

---
+++ backend/src/middleware/multer.middleware.js
@@ -0,0 +1,29 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// filefilter to only allow image files
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|webp/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLocaleLowerCase(),
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed"));
+  }
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 },
+}); // limit file size to 5MB
