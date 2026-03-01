2aec10bf3952757cf796eaea65772445a0fbd87e
Talha Zahid <italhazahid@gmail.com>
2026-03-01 14:40:53 +0500

    Added throw e for error handling

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -12,7 +12,7 @@ const syncUser = inngest.createFunction(
     try {
       await connectDB();
       const { id, email_addresses, first_name, last_name, image_url } =
-      event.data;
+        event.data;
       //for mongoose, we need to create a new instance of the model and then save it to the database
       const newUser = {
         clerkId: id,
@@ -20,12 +20,12 @@ const syncUser = inngest.createFunction(
         name: `${first_name || ""} ${last_name || ""}`.trim() || "User",
         imageUrl: image_url,
       };
-      
+
       //Now we can create a new user document and save it to the database
       await User.create(newUser);
-    }
-    catch(e){
+    } catch (e) {
       console.error("Failed to sync user", e);
+      throw e;
     }
   },
 );
@@ -36,14 +36,13 @@ const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
-    try{
-
+    try {
       await connectDB();
       const { id } = event.data;
       await User.findOneAndDelete({ clerkId: id });
-    }
-    catch(e){
+    } catch (e) {
       console.error("Failed to delete user", e);
+      throw e;
     }
   },
 );
@@ -54,20 +53,19 @@ const updateUserInDB = inngest.createFunction(
   { id: "update-user-in-db" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
-    try{
-
+    try {
       await connectDB();
       const { id, email_addresses, first_name, last_name, image_url } =
-      event.data;
+        event.data;
       const updatedUser = {
         email: email_addresses[0]?.email_address,
         name: `${first_name || ""} ${last_name || ""}`.trim() || "User",
         imageUrl: image_url,
       };
       await User.findOneAndUpdate({ clerkId: id }, updatedUser, { new: true }); //{new:true} option is used to return the updated document instead of the old one
-    }
-    catch(e){
+    } catch (e) {
       console.error("Failed to update user", e);
+      throw e;
     }
   },
 );
