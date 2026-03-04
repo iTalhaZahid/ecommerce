eb7600ad8fbb0cb002452a244b41d69276866c79
Talha Zahid <italhazahid@gmail.com>
2026-03-04 15:32:30 +0500

    Added Order ,Customer and Stats Route

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -3,6 +3,10 @@ import {
   createProduct,
   getAllProducts,
   updateProduct,
+  getAllCustomers,
+  updateOrderStatus,
+  getAllOrders,
+  getDashboardStats,
 } from "../controllers/admin.controller.js";
 import { adminOnly, protectRoute } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
@@ -14,5 +18,11 @@ router.use(protectRoute, adminOnly); // Apply protectRoute and adminOnly middlew
 router.post("/products", upload.array("images", 3), createProduct); // Use multer middleware to handle file uploads, allowing up to 3 images per product
 router.get("/products", getAllProducts);
 router.put("/products/:id", upload.array("images", 3), updateProduct);
+
+router.get("/customers", getAllCustomers);
+router.patch("/orders/:id/status", updateOrderStatus);
+
+router.get("/orders", getAllOrders);
+router.get("/stats", getDashboardStats);
 
 export default router;
