10d9753956f6e108538760bdea387dc94fa3b6cc
Talha Zahid <italhazahid@gmail.com>
2026-03-09 16:48:32 +0500

    Added Dashboard Layout

---
+++ admin/src/layouts/DashboardLayout.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from "react-router";
+import SideBar from "../components/SideBar";
+import NavBar from "../components/NavBar";
+
+function DashboardLayout() {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input
+        id="my-drawer"
+        type="checkbox"
+        className="drawer-toggle"
+        defaultChecked
+      />
+      <div className="drawer-content">
+        <NavBar />
+        <main className="p-6">
+          <Outlet />
+        </main>
+      </div>
+      <SideBar />
+    </div>
+  );
+}
+
+export default DashboardLayout;
