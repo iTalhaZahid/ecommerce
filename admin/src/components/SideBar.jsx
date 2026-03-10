f01368d9df7eabbcdbbd1457c168deb6bbfd5ce6
Talha Zahid <italhazahid@gmail.com>
2026-03-10 18:17:35 +0500

    adjusted Width of Drwer When Closed

--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -1,28 +1,26 @@
 import { useUser } from "@clerk/clerk-react";
 import { ShoppingBagIcon } from "lucide-react";
 import { Link, useLocation } from "react-router";
-import { NAVIGATION } from "./NavBar";
-function SideBar() {
+import { NAVIGATION } from "./Navbar";
+
+function Sidebar() {
   const location = useLocation();
   const { user } = useUser();
+
   return (
     <div className="drawer-side is-drawer-close:overflow-visible">
-      <label
-        htmlFor="my-drawer"
-        className="drawer-overlay"
-        aria-label="close sidebar"
-      />
-      <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
+      <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+
+      <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-16 is-drawer-open:w-64">
         <div className="p-4 w-full">
           <div className="flex items-center gap-3">
-            <div className="size-10 bg-primary rounded-xl justify-center items-center flex shrink-0">
+            <div className="size-10 bg-primary rounded-xl flex items-center justify-center shrink-0">
               <ShoppingBagIcon className="w-6 h-6 text-primary-content" />
             </div>
-            <span className="text-xl font-bold is-drawer-close:hidden">
-              Admin
-            </span>
+            <span className="text-xl font-bold is-drawer-close:hidden">Admin</span>
           </div>
         </div>
+
         <ul className="menu w-full grow flex flex-col gap-2">
           {NAVIGATION.map((item) => {
             const isActive = location.pathname === item.path;
@@ -30,7 +28,9 @@ function SideBar() {
               <li key={item.path}>
                 <Link
                   to={item.path}
-                  className={`is-drawer-close:tooltip-right ${isActive ? "bg-primary text-primary-content" : ""}`}
+                  className={`is-drawer-close:tooltip is-drawer-close:tooltip-right 
+                    ${isActive ? "bg-primary text-primary-content" : ""}
+                  `}
                 >
                   {item.icon}
                   <span className="is-drawer-close:hidden">{item.name}</span>
@@ -39,9 +39,10 @@ function SideBar() {
             );
           })}
         </ul>
+
         <div className="p-4 w-full">
           <div className="flex items-center gap-3">
-            <div className="avatar shrink-0">
+            <div className="avatar shrink-0 is-drawer-close:mx-auto">
               {user?.imageUrl ? (
                 <img
                   src={user?.imageUrl}
@@ -52,10 +53,14 @@ function SideBar() {
                 <span className="font-semibold bg-primary text-primary-content rounded-full w-10 h-10 flex items-center justify-center">{user?.fullName?.[0]}</span>
               )}
             </div>
+
             <div className="flex-1 min-w-0 is-drawer-close:hidden">
-              <p className="text-sm font-semibold truncate">{user?.fullName}</p>
+              <p className="text-sm font-semibold truncate">
+                {user?.firstName} {user?.lastName}
+              </p>
+
               <p className="text-xs opacity-60 truncate">
-                {user?.emailAddresses[0].emailAddress}
+                {user?.emailAddresses?.[0]?.emailAddress}
               </p>
             </div>
           </div>
@@ -64,5 +69,4 @@ function SideBar() {
     </div>
   );
 }
-
-export default SideBar;
+export default Sidebar;
