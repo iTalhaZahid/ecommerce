c16d5bdbc02a8e59dd03736fe37d78b2d3181f7b
Talha Zahid <italhazahid@gmail.com>
2026-03-15 15:01:47 +0500

    Setting Up Clerk

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
 import { Stack } from "expo-router";
 import {
   QueryClient,
@@ -6,11 +8,17 @@ import {
 import "../global.css";
 export default function RootLayout() {
   const queryClient = new QueryClient()
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+  if (!publishableKey) {
+    throw new Error('Add your Clerk Publishable Key to the .env file')
+  }
 
   return (
-
-    <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={{headerShown:false}}/>
-    </QueryClientProvider>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </QueryClientProvider>
+    </ClerkProvider >
   )
 }
